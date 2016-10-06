a41448999487905bce9a851fdaf4449483f8346e
aadikulkarni91 <aadikulkarni91@gmail.com>
2016-10-06 17:53:01 -0500

    initial commit

---
+++ src/java/FindHashPassord/FindHashPassord.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FindHashPassord;
+
+import uta.cse4361.businessobjects.AdvisorAccount;
+
+/**
+ *
+ * @author Aaditya
+ */
+public class FindHashPassord {
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	System.out.println(AdvisorAccount.hashPassword("aaditya"));
+    }
+//hashPassword("aaditya");
+    
+}
